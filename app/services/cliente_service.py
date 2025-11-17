b48b2496e94e5df5d2d853283f446f02ac00ab5f
AlejandraPerez-dotcom <mariaalejandraperez004@gmail.com>
2025-11-17 13:50:57 -0500

    Actualizo archivo cliente_service.py

---
+++ app/services/cliente_service.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+from repository.cliente_repository import ClienteRepository
+from domain.cliente_model import ClienteCreate,ClienteResponse
+from utils.password import hash_password, verify_password
+
+
+class ClienteService:
+    def __init__(self, db: Session):
+        self.repository = ClienteRepository(db)
+
+    def registrar_cliente(self, data: ClienteCreate):
+
+        if self.repository.get_by_email(data.email):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="El correo electronico ya esta registrado."
+            )
+        
+        hashed = hash_password(data.password)
+
+        cliente =self.repository.create_cliente(data, hashed)
+
+        return {
+            "mensaje": "Registro exitoso. Bienvenido a la plataforma.",
+            "data": ClienteResponse.from_orm(cliente),
+            "success": True
+        }
+
