6815ceee19dba70d77a1ea9aa99c6b0142845a28
AlejandraPerez-dotcom <mariaalejandraperez004@gmail.com>
2025-11-17 13:49:56 -0500

    Actualizo archivo cliente_repository.py

---
+++ app/repository/cliente_repository.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Session
+from database import ClienteDB
+from domain.cliente_model import ClienteCreate
+from datetime import datetime
+
+class ClienteRepository:
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_by_email(self, email: str) -> ClienteDB:
+        return self.db.query(ClienteDB).filter(ClienteDB.email == email).first()
+    
+    def create_cliente(self, cliente: ClienteCreate, hashed_password: str) -> ClienteDB:
+        nuevo = ClienteDB(
+            nombre=cliente.nombre,
+            apellido=cliente.apellido,
+            email=cliente.email,
+            password=hashed_password,
+            telefono=cliente.telefono, 
+            direccion=cliente.direccion,
+            fecha_registro=datetime.utcnow()
+        )
+
+        nuevo.password = hashed_password
+
+        self.db.add(nuevo)
+        self.db.commit()
+        self.db.refresh(nuevo)
+        return nuevo
