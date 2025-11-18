57d75bf122aca9aaeedf555e9e190d9183c47f82
AlejandraPerez-dotcom <mariaalejandraperez004@gmail.com>
2025-11-18 18:44:20 -0500

    Actualizo archivo routers.py

--- app/config/routers.py
+++ app/config/routers.py
@@ -1,9 +1,11 @@
 from api import Registro_api
 from api import Inicio_api
 from api import Perfil_api
+from api import Reserva_api
 
 ROUTERS = [
     Registro_api.router, 
     Inicio_api.router,
     Perfil_api.router,
+    Reserva_api.router,
 ]
