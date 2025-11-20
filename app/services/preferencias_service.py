45b10a79ef41117c789ce80e46b5a8abeb4d5989
Wilder Pardo <wdpardo@uts.edu.co>
2025-11-20 15:28:49 -0500

    comparto archivo de preferencias_service.py

---
+++ app/services/preferencias_service.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+
+from repository.preferencias_repository import PreferenciasRepository
+from domain.preferencias_model import PreferenciasBase, PreferenciasResponse
+
+class PreferenciasService:
+
+    def __init__(self, db: Session):
+        self.repository = PreferenciasRepository(db)
+
+    def get_preferencias(self, id_cliente: int):
+        prefs = self.repository.get_by_cliente(id_cliente)
+
+        if not prefs:
+            raise HTTPException(
+                status_code=200,
+                detail="No existen preferencias registradas para este usuario."
+            )
+        return PreferenciasResponse.from_orm(prefs)
+
+    def save_preferencias(self, id_cliente: int, datos: PreferenciasBase):
+        prefs = self.repository.save(id_cliente, datos)
+        return PreferenciasResponse.from_orm(prefs)
