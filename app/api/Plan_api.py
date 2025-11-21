b83ab6767409c197a623a9f7c005d447b54a0bc0
Wilder Pardo <wdpardo@uts.edu.co>
2025-11-21 14:34:33 -0500

    comparto archivo Plan_api.py

---
+++ app/api/Plan_api.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from services.plan_service import PlanService
+from domain.plan_model import ResponsePlanList, ResponsePlanDetalle
+
+router = APIRouter(prefix="/reservas", tags=["Planes"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/planes", response_model=ResponsePlanList)
+def obtener_planes(db: Session = Depends(get_db)):
+    service = PlanService(db)
+    return service.listar_planes()
+
+
+@router.get("/planes/{idPlan}", response_model=ResponsePlanDetalle)
+def obtener_plan_por_id(idPlan: int, db: Session = Depends(get_db)):
+    service = PlanService(db)
+    return service.obtener_plan_por_id(idPlan)
