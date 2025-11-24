7e850c3a566536bb46d2cd0f42298d0b41cf1684
AlejandraPerez-dotcom <mariaalejandraperez004@gmail.com>
2025-11-23 22:44:17 -0500

    Subiendo archivos de la HU-001

---
+++ app/repository/Registrar_repository.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+from app.models.ClienteBD import ClienteDB
+from app.domain.Registrar_model import ClienteCreate
+from datetime import datetime
+
+class ClienteRepository:
+
+    def _init_(self, db: Session):
+        self.db = db
+
+    def get_by_email(self, email: str) -> ClienteDB:
+        return self.db.query(ClienteDB).filter(ClienteDB.email == email).first()
+    
+    def create_cliente(self, cliente: ClienteCreate, hashed_password: str) -> ClienteDB:
+        nuevo = ClienteDB(
+            nombre=cliente.nombre,
+            apellido=cliente.apellido,
+            email=cliente.email,
+            password=hashed_password,
+            telefono=cliente.telefono, 
+            direccion=cliente.direccion,
+            fecha_registro=datetime.utcnow()
+        )
+
+        nuevo.password = hashed_password
+
+        self.db.add(nuevo)
+        self.db.commit()
+        self.db.refresh(nuevo)
+        return nuevo
