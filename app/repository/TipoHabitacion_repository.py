d1949d9aeda3e7b1832dd5d8faa929fd36773ee2
Wilder Pardo <wdpardo@uts.edu.co>
2025-11-27 07:33:26 -0500

    Actualizando archivos

--- app/repository/TipoHabitacion_repository.py
+++ app/repository/TipoHabitacion_repository.py
@@ -1,13 +1,14 @@
 from sqlalchemy.orm import Session
-from app.models import TipoHabitacionDB, ReservaDB
+from app.models import HabitacionesDB
+from app.models.ReservasBD import ReservasDB
 from app.domain.TipoHabitacion_model import TipoHabitacionCreate
 
 class TipoHabitacionRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    def create_tipo(self, tipo_data: TipoHabitacionCreate) -> TipoHabitacionDB:
-        tipo = TipoHabitacionDB(
+    def create_tipo(self, tipo_data: TipoHabitacionCreate) -> HabitacionesDB:
+        tipo = HabitacionesDB(
             nombre=tipo_data.nombre,
             descripcion=tipo_data.descripcion,
             descripcion_plan=tipo_data.descripcion_plan,
@@ -20,13 +21,13 @@ class TipoHabitacionRepository:
         self.db.refresh(tipo)
         return tipo
 
-    def get_by_id(self, id_tipo: int) -> TipoHabitacionDB:
-        return self.db.query(TipoHabitacionDB).filter(TipoHabitacionDB.id_tipo == id_tipo).first()
+    def get_by_id(self, id_tipo: int) -> HabitacionesDB:
+        return self.db.query(HabitacionesDB).filter(HabitacionesDB.id_tipo == id_tipo).first()
 
-    def get_by_nombre(self, nombre: str) -> TipoHabitacionDB:
-        return self.db.query(TipoHabitacionDB).filter(TipoHabitacionDB.nombre == nombre).first()
+    def get_by_nombre(self, nombre: str) -> HabitacionesDB:
+        return self.db.query(HabitacionesDB).filter(HabitacionesDB.nombre == nombre).first()
 
-    def update_tipo(self, id_tipo: int, tipo_data: dict) -> TipoHabitacionDB:
+    def update_tipo(self, id_tipo: int, tipo_data: dict) -> HabitacionesDB:
         tipo = self.get_by_id(id_tipo)
         for key, value in tipo_data.items():
             setattr(tipo, key, value)
@@ -36,7 +37,7 @@ class TipoHabitacionRepository:
 
     def delete_tipo(self, id_tipo: int) -> bool:
         tipo = self.get_by_id(id_tipo)
-        reservas = self.db.query(ReservaDB).filter(ReservaDB.id_tipoHabitacion == id_tipo).count()
+        reservas = self.db.query(ReservasDB).filter(ReservasDB.id_tipoHabitacion == id_tipo).count()
         if reservas > 0:
             return False
         self.db.delete(tipo)
