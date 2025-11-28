76683360d9c2f0ca5d14e2b860ca89c99e55523a
AlejandraPerez-dotcom <mariaalejandraperez004@gmail.com>
2025-11-28 16:08:06 -0500

    Actualizo archivo ReservasBD.py

--- app/models/ReservasBD.py
+++ app/models/ReservasBD.py
@@ -1,22 +1,25 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from datetime import datetime
 from sqlalchemy.orm import relationship
 from app.models.Base import Base
 
+
 class ReservasDB(Base):
     __tablename__ = "reservas"
+
     id_reserva = Column(Integer, primary_key=True)
     id_cliente = Column(Integer, ForeignKey("clientes.id_cliente"), nullable=False)
     id_tipoHabitacion = Column(Integer, ForeignKey("habitaciones.id_tipoHabitacion"), nullable=False)
     fecha_reserva = Column(DateTime, nullable=False)
     check_in = Column(Date, nullable=False)
     check_out = Column(Date, nullable=False)
-    habitacion = Column(String, nullable=False)
+    habitacion = Column(String, nullable=False)  # nombre interno o código
+    nombre_habitacion = Column(String, nullable=False)  # ← NUEVO CAMPO
     plan = Column(String, nullable=False)
     valor_total = Column(Integer, nullable=False)
     estado_reserva = Column(String, nullable=False)
 
     cliente = relationship("ClienteDB", back_populates="reservas")
-    habitacion = relationship("HabitacionesDB", back_populates="habitaciones" )
